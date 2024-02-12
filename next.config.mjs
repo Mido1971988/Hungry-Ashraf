a2752f08b5cd667be389f6aa73679678767ea2cf
mohamed-hussein88 <mohammed.soliman88@gmail.com>
2024-02-12 04:52:39 +0300

    daily

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/Hungry-Ashraf",
+  // basePath: "/Hungry-Ashraf",
 };
 
-module.exports = nextConfig;
+export default nextConfig;
